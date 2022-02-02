f67f9ec4b730d8e691b53010329d801a8b270e02
JoostdeJager <46704554+JoostdeJager@users.noreply.github.com>
2022-02-02 15:12:47 +0100

    initial commit

---
+++ lib/platem.ex
@@ -0,0 +1,18 @@
+defmodule Platem do
+  @moduledoc """
+  Documentation for `Platem`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Platem.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/platem_test.exs
@@ -0,0 +1,8 @@
+defmodule PlatemTest do
+  use ExUnit.Case
+  doctest Platem
+
+  test "greets the world" do
+    assert Platem.hello() == :world
+  end
+end
