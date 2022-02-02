f611a47ef7f29a2dcdc59607180f8ddc6924ad78
JoostdeJager <46704554+JoostdeJager@users.noreply.github.com>
2022-02-02 22:55:50 +0100

    removed save function from page module

--- lib/pages/page.ex
+++ lib/pages/page.ex
@@ -1,10 +1,3 @@
 defmodule Platem.Page do
   defstruct [:html, :folder, :name, :published, :path]
-
-  def save_page(page) do
-    File.write(
-      Path.join([Map.get(page, :folder), '#{Map.get(page, :name)}.html.heex']),
-      page.html
-    )
-  end
 end
